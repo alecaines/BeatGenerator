22b5af8b83ef5142aeed97bcce0eb610e975e1b3
M <alecaines@gmail.com>
2019-11-18 00:30:20 -0500

    trying to push

--- beatGenerator.py
+++ beatGenerator.py
@@ -44,32 +44,28 @@ class BEATGENERATOR(object):
 
 
     def main(self):
-        osys = platform.system()
         # I (Alexander) am unsure if ffmpeg works differently on different operating systems. So to be safe, I'm deferring to working with Windows.
         # I will check later if this works on linux. If you wish to check if the program runs on a MAC, install ffmpeg off the site I linked in the
         # else statement. After you have installed ffmpeg, replace 'Windows' with 'Darwin'
-        if osys == 'Windows':
-            if os.path.exists('../songs'): #for running on a windows machine
-                mp3_files = gb.glob('../songs/*.mp3') #list of mp3 file addresses in a folder called songs sitting outside of this directory
-                
-                for i in range(len(mp3_files)):
-                    #the following returns an np array (vector) representing one mp3 file.
-                    # I believe each element represents audio data at one millisecond in the audio file
-                    # but I am not entirely sure. 
-                    frame_rate, vector = self.transformData(mp3_files[i]) #Note, the framerate is in milliseconds
-                    filename = str(mp3_files[i])[9:] + ".txt"
-                    self.proofOfWork(vector, filename) #should be a global array
-
-            else:
-                f = "Hip Hop SFX.mp3"
-                #the following returns an np array (vector) representing one mp3 file
-                frame_rate, vector = self.transformData(f) #framerate is in milliseconds
-                filename = str(f)+ ".txt"
-                self.proofOfWork(vector, filename) #should be a global array
+        
+        if os.path.exists('../songs'): #for running on a windows machine
+            mp3_files = gb.glob('../songs/*.mp3') #list of mp3 file addresses in a folder called songs sitting outside of this directory
             
+            for i in range(len(mp3_files)):
+                #the following returns an np array (vector) representing one mp3 file.
+                # I believe each element represents audio data at one millisecond in the audio file
+                # but I am not entirely sure. 
+                frame_rate, vector = self.transformData(mp3_files[i]) #Note, the framerate is in milliseconds
+                filename = str(mp3_files[i])[9:] + ".txt"
+                self.proofOfWork(vector, filename) #should be a global array
+
         else:
-            print("Please install  ffmpeg for "+osys+". http://www.ffmpeg.org/download.html")
-            print("Support for " + osys + " will be implemented soon")
+            f = "Hip Hop SFX.mp3"
+            #the following returns an np array (vector) representing one mp3 file
+            frame_rate, vector = self.transformData(f) #framerate is in milliseconds
+            filename = str(f)+ ".txt"
+            self.proofOfWork(vector, filename) #should be a global array
+            
 
 if __name__ == "__main__":
     BEATGENERATOR().main()
