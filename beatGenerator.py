9d2de75c36b763f3dc5603d002c5d0ac1d4607b4
alecaines <alecaines@gmail.com>
2019-12-07 12:20:17 -0500

    first commit

--- beatGenerator.py
+++ beatGenerator.py
@@ -2,24 +2,18 @@
 # Description: This file retrieves and preprocesses song data for beat generation
 
 import pydub #allows for manipulation of audio
-import scipy
 import numpy as np
 import glob as gb # glob lists the files of a certain filetype in a folder specified by the programmer
 import os
-import platform #for determining the operating system local to the machine
 
 class BEATGENERATOR(object):
     def __init__(self):
         pass
 
     # converts mp3 to numpy array
-    def transformData(self, f, normalized = True, t = 3):
-        duration = t*1000 #converts to miliseconds
-
+    def transformData(self, f):
         #retrieves audio
         a = pydub.AudioSegment.from_mp3(file = f)
-        if a.duration_seconds > t:
-            a = a[:duration]
 
         # converts mp3 data to numpy array
         y = np.array(a.get_array_of_samples())
@@ -28,16 +22,15 @@ class BEATGENERATOR(object):
         if a.channels == 2:
             y = y.reshape((-1,2))
         
-        #normalizes elements
-        if normalized:
-            #depending on what we use for our activation function, we may want to remove the zeroes and ones
-            return a.frame_rate, np.float32(y)/2**15
-        else:
-            return a.frame_rate, y
+        #normalizes elements and return
+        #depending on what we use for our activation function, we may want to remove the zeroes and ones
+        return np.float32(y)/2**15
     
     # writes quantified audio data to txt
-    def proofOfWork(self, v, filename):
-        f = open(filename, "w+")
+    def writeFile(self, v, filename, folder):
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        f = open(folder + "/" + filename, "w+")
         for i in range(len(v)):
             f.write(str(v[i]))
         f.close()
@@ -47,7 +40,6 @@ class BEATGENERATOR(object):
         # I (Alexander) am unsure if ffmpeg works differently on different operating systems. So to be safe, I'm deferring to working with Windows.
         # I will check later if this works on linux. If you wish to check if the program runs on a MAC, install ffmpeg off the site I linked in the
         # else statement. After you have installed ffmpeg, replace 'Windows' with 'Darwin'
-        
         if os.path.exists('../songs'): #for running on a windows machine
             mp3_files = gb.glob('../songs/*.mp3') #list of mp3 file addresses in a folder called songs sitting outside of this directory
             
@@ -55,17 +47,20 @@ class BEATGENERATOR(object):
                 #the following returns an np array (vector) representing one mp3 file.
                 # I believe each element represents audio data at one millisecond in the audio file
                 # but I am not entirely sure. 
-                frame_rate, vector = self.transformData(mp3_files[i]) #Note, the framerate is in milliseconds
+                vector = self.transformData(mp3_files[i]) #Note, the framerate is in milliseconds
                 filename = str(mp3_files[i])[9:] + ".txt"
-                self.proofOfWork(vector, filename) #should be a global array
+                self.writeFile(vector, filename, "../vectorizedAudio") #should be a global array
 
         else:
             f = "Hip Hop SFX.mp3"
             #the following returns an np array (vector) representing one mp3 file
-            frame_rate, vector = self.transformData(f) #framerate is in milliseconds
+            vector = self.transformData(f) #framerate is in milliseconds
             filename = str(f)+ ".txt"
-            self.proofOfWork(vector, filename) #should be a global array
+            self.writeFile(vector, filename, "../vectorizedAudio") #should be a global array
             
+        # else:
+        #     print("Please install  ffmpeg for "+osys+". http://www.ffmpeg.org/download.html")
+        #     print("Support for " + osys + " will be implemented soon")
 
 if __name__ == "__main__":
     BEATGENERATOR().main()
